0684d4fa43619f7621325cbbf2ed8806cc9fa8fa
LazyTarget <peter.aslund@hotmail.com>
2014-10-29 09:44:05 +0100

    Added HttpRequestExtensions.cs

---
+++ PocketAPI/Components/HttpRequestExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+
+namespace PocketAPI
+{
+    public static class HttpRequestExtensions
+    {
+        public static T WithUrl<T>(this T request, string url)
+            where T : IHttpHelperRequest
+        {
+            request.Url = url;
+            return request;
+        }
+
+        public static T WithMethod<T>(this T request, string method)
+            where T : IHttpHelperRequest
+        {
+            request.Method = method;
+            return request;
+        }
+
+        public static T WithData<T>(this T request, object data)
+            where T : IHttpHelperRequest
+        {
+            request.Data = data;
+            return request;
+        }
+
+        public static T WithHeaders<T>(this T request, WebHeaderCollection headers)
+            where T : IHttpHelperRequest
+        {
+            request.Headers = headers;
+            return request;
+        }
+
+    }
+}
